f77e22e2126b649a7e1d92b69c8744475a0c9b1e
Andreas Arnesson <Andreas_osby@hotmail.com>
2024-02-01 09:02:25 +0100

    open link

--- scripts/open-link.js
+++ scripts/open-link.js
@@ -6,13 +6,13 @@ let openLink = {
     action: function () {
         let addPart = window.sessionStorage.getItem("addToStudentUrlPath");
 
-        let pattern_inspect = /https?:\/\/w+.student.bth.se\/~\w{4}\d{2}\/dbwebb-kurser\/\w+\/me\//i;
+        let pattern_inspect = /https?:\/\/w+.student.bth.se\/~\w{4}\d{2}\/dbwebb-kurser\/\w+\//i;
         // let pattern_inspect = /https?:\/\/w+.student.bth.se\/~\w{4}\d{2}\/dbwebb-kurser.*/i;
         let iframe = document.getElementById("speedgrader_iframe").contentWindow.document;
         let content = iframe.getElementById("submission_preview");
         let match_inspect = content.innerText.match(pattern_inspect);
         if (match_inspect !== null) {
-            let url = match_inspect + (addPart ? addPart : "");
+            let url = `${match_inspect}me/${addPart ? addPart : ""}`;
             window.open(url, '_blank');
         }
         exit.actionRemove();
