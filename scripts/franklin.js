184d063055d2b85cf18ed630702319c33472138b
AndreasArne <andreas_osby@hotmail.com>
2024-09-12 14:43:15 +0200

    frankling funkar med nya kommentarsrutan

--- scripts/franklin.js
+++ scripts/franklin.js
@@ -6,7 +6,7 @@ let franklin = {
     name: "Skriv kommentar",
     action: function () {
         if (!document.getElementById("feedbackText")) {
-            let script = document.getElementById("speed_grader_comment_textarea") ? 'speed-franklin.js' : 'franklin.js';
+            let script = document.getElementById("speed_grader_comment_textarea_mount_point") ? 'speed-franklin.js' : 'franklin.js';
 
             document.body.appendChild(document.createElement('script')).src = 'https://booklets.emilfolino.se/' + script;
 
